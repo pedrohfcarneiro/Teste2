71c208beb414a30cf388d05ffc0a1507171d5983
phtriplox <phtriplox8090@gmail.com>
2017-08-14 23:54:36 -0300

    p

---
+++ TEST_ Flying Camera Menu/Assets/MenuCamControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamControl : MonoBehaviour {
+    public Transform GreenPos, BluePos, RedPos;
+    public Transform DestPos;
+
+
+
+
+    void Start ()
+    {
+        DestPos = GreenPos;
+        	
+	}
+	
+	
+	void Update ()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, DestPos.position, 0.01f);
+        transform.rotation = Quaternion.Slerp(transform.localRotation, DestPos.rotation, 0.01f);
+        	
+	}
+
+    public void MoveToGreen ()
+    {
+        DestPos = GreenPos;
+    }
+
+    public void MoveToBlue ()
+    {
+        DestPos = BluePos;
+    }
+
+    public void MoveToRed ()
+    {
+        DestPos = RedPos;
+    }
+}
---
+++ TEST_ Flying Camera Menu/Assets/Test 1/Test1_Script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1_Script : MonoBehaviour {
+    public Transform GamePos;
+    public Camera MainCam;
+    public float smooth = 5f;
+    public float zoom = 20f;
+
+
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, GamePos.position, 0.1f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, GamePos.rotation, 0.1f);
+
+        MainCam.fieldOfView = Mathf.Lerp(MainCam.fieldOfView, zoom, Time.deltaTime * smooth);
+    }
+
+
+
+
+
+}
